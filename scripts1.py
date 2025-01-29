be3ae3152613ef5ad7d907279b4b297643f38a2a
Kowshik MK <113271988+Kowshik-mk@users.noreply.github.com>
2025-01-29 07:24:33 +0530

    Update scripts1.py

--- scripts1.py
+++ scripts1.py
@@ -10,7 +10,7 @@ parameter1= str(sys.argv[5])
 parameter2 = str(sys.argv[6])
 
 print( "the toke value is")
-def trigger_workflow(workflowname,parameter1,parameter2):
+def trigger_workflow(work_Flowname,parameter1,para_meter2):
 
       headers = {
         "Accept": "application/vnd.github.v3+json",
